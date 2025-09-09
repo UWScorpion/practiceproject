43a9adbcdf82c674d4e600899977588c9d5a5e5f
Alex <zdfelix@gmail.com>
2025-09-08 19:44:44 -0700

    add todolist

--- my-practice-app/app/todolist/page.tsx
+++ my-practice-app/app/todolist/page.tsx
@@ -1,7 +1,56 @@
 "use client";
+import { useState } from "react";
 
+const DEFAULT_ITEMS = [
+  { id: "1", name: "apple", checked: false },
+  { id: "2", name: "banana", checked: false },
+  { id: "3", name: "peach", checked: false },
+];
 const TodoListPage = () => {
-  return <div>Todo List Page</div>;
+  const [items, setItems] = useState(DEFAULT_ITEMS);
+  const [addItem, setAddItem] = useState("");
+  const addItemToList = () => {
+    if (addItem === "") return;
+    const newitems = [
+      { id: "" + (items.length + 1), name: addItem, checked: false },
+      ...items,
+    ];
+    setItems(newitems);
+  };
+  const setChecked = (idx: number) => {
+    const newItem = { ...items[idx], checked: !items[idx].checked };
+    const newItems = [...items];
+    newItems[idx] = newItem;
+    setItems(newItems);
+  };
+
+  const deleteItems = () => {
+    const newitems = items.filter(i=>!i.checked);
+    setItems(newitems);
+  };
+  return (
+    <>
+      <input
+        type="text"
+        value={addItem}
+        onChange={(e) => setAddItem(e.target.value)}
+      ></input>
+      <button onClick={() => addItemToList()}>add</button>
+      <ul>
+        {items.map((i, idx) => (
+          <li key={i.id}>
+            <input
+              type="checkbox"
+              checked={i.checked}
+              onChange={() => setChecked(idx)}
+            ></input>
+            {i.name}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => deleteItems()}>delete</button>
+    </>
+  );
 };
 
 export default TodoListPage;
